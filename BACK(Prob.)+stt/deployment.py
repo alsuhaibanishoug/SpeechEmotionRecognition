3a5292ab0afa2b57b594988c0d2c5eb6b78707a2
Shoug <92891184+alsuhaibanishoug@users.noreply.github.com>
2023-01-28 16:36:40 +0300

    Upload deployment.py

---
+++ BACK(Prob.)+stt/deployment.py
@@ -0,0 +1,157 @@
+
+from flask import Flask, request , render_template
+import os
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import librosa
+from pydub import AudioSegment
+#pip install speechrecognition
+import speech_recognition as sr
+from pydub.silence import split_on_silence
+#pip install arabic_reshaper
+import arabic_reshaper
+#pip install python-bidi
+from bidi.algorithm import get_display
+
+
+
+
+app = Flask(__name__, template_folder='template')
+
+# create a speech recognition object
+r = sr.Recognizer()
+
+#logic starts
+def extract_features(data, sample_rate):
+    # ZCR
+    result = np.array([])
+    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+    result=np.hstack((result, zcr)) # stacking horizontally
+
+    # Chroma_stft
+    stft = np.abs(librosa.stft(data))
+    chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, chroma_stft)) # stacking horizontally
+
+    # MFCC
+    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mfcc)) # stacking horizontally
+
+    # Root Mean Square Value
+    rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
+    result = np.hstack((result, rms)) # stacking horizontally
+
+    # MelSpectogram
+    mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mel)) # stacking horizontally
+
+    return result
+
+def get_features(path):
+    # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
+    data, sample_rate = librosa.load(path, offset=0.4)
+    data = data.T
+
+    result = extract_features(data,sample_rate)
+
+    return result
+
+def checker(sent_audio):
+
+    msg=get_features(sent_audio)
+
+    data = pd.DataFrame(msg)
+    data = np.expand_dims(data, axis=0)
+
+    model = tf.keras.models.load_model('/home/shougsu/mysite/5_Emotion_Model_cnn_87.h5', compile=False)
+    prediction= model.predict(data)
+
+    all_emotion=[str(100*np.max(n)) for n in prediction[0]]
+    all_emotion=dict(enumerate(all_emotion))
+
+    classnames={0:'angry',1:'happy',2:'neutral',3:'sad',4:'surprise'}
+    all_emotion=dict((classnames[key], value) for (key, value) in all_emotion.items())
+
+    return all_emotion
+
+# a function that splits the audio file into chunks
+# and applies speech recognition
+def audio_transcription(path, lang):
+
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)
+    # split audio sound where silence is 1 second or more and get chunks
+    chunks = split_on_silence(sound,
+        min_silence_len = 1000,
+        silence_thresh = sound.dBFS-16,
+        keep_silence=200,
+    )
+
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                if(lang == 'ar'):
+                    text = r.recognize_google(audio_listened, language = 'ar-SA')
+                else:
+                    text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f" {text.capitalize()},"
+                whole_text += text
+
+    if(lang == 'ar'):
+        reshaped_text = arabic_reshaper.reshape(whole_text)    # correct its shape
+        whole_text = get_display(reshaped_text)                # correct its direction
+
+
+    whole_text = whole_text[:-1]
+    whole_text+='.'
+    return whole_text
+
+
+@app.route('/',  methods=['GET','POST'])
+def home():
+    if 'file' in request.files:
+        uploaded_file = request.files['file']
+        if uploaded_file != '':
+            _, ext = os.path.splitext(uploaded_file.filename)
+            if ext == ".mp3":
+                sound = AudioSegment.from_mp3(uploaded_file)
+                wav_form = sound.export(format="wav")
+                uploaded_file = wav_form
+            elif ext == '.avi':
+                sound = AudioSegment.from_file(uploaded_file, 'avi')
+                wav_form = sound.export(format="wav")
+                uploaded_file = wav_form
+            elif ext == '.m4a':
+                sound = AudioSegment.from_file(uploaded_file, 'm4a')
+                wav_form = sound.export(format="wav")
+                uploaded_file = wav_form
+
+            responce = checker(uploaded_file)
+
+            if _ == 'ar':
+                text = audio_transcription(uploaded_file, 'ar')
+            else:
+                text = audio_transcription(uploaded_file, 'en')
+
+            responce['text']= text
+
+            return responce
+
+    return render_template('index.html')
